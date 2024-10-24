4bdf8f0a0a685b7871ebf9a8bbc1eb8a4f338b42
ShelpAm <shelpam@qq.com>
2024-10-24 14:15:58 +0800

    refactor: modularize main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <chrono>
 #include <fast_io.h>
+#include <fast_io_dsal/span.h>
 #include <random>
 #include <string>
 #include <thread>
 #include <vector>
 
 struct Settings {
+  std::size_t n, m; // Size of the game map.
   std::size_t fps;
   double live_probability;
   std::size_t on_minimum;
@@ -14,15 +16,14 @@ struct Settings {
   std::size_t off_maximum;
 };
 
-namespace fast_io::io {
-void scan(fast_io::ibuffer_view &ibf, Settings &settings)
+void scan(fast_io::ibuffer_view &ibf, Settings &s)
 {
-  scan(ibf, settings.fps);
+  scan(ibf, s.n, s.m);
+  scan(ibf, s.fps);
   // scan(ibf, settings.live_probability);
-  scan(ibf, settings.on_minimum, settings.on_maximum);
-  scan(ibf, settings.off_minimum, settings.off_maximum);
+  scan(ibf, s.on_minimum, s.on_maximum);
+  scan(ibf, s.off_minimum, s.off_maximum);
 }
-} // namespace fast_io::io
 
 // With probability of p to be true.
 auto gen_bool(double p) -> bool
@@ -44,85 +45,93 @@ auto gen_bool(double p) -> bool
   return random_value < p;
 }
 
+using state_t = std::vector<std::string>;
+
+auto initialize_state(Settings const &s, fast_io::ibuffer_view &ibf) -> state_t
+{
+  state_t state(s.n, std::string(s.m, '.'));
+
+  state_t state_map;
+  std::string buf;
+  while (scan<true>(ibf, buf)) {
+    state_map.push_back(buf);
+  }
+  auto const width{state_map[0].size()};
+  auto const height{state_map.size()};
+
+  // Initialize the state.
+  auto const off_x{(s.n - height) / 2};
+  auto const off_y{(s.m - width) / 2};
+  for (std::size_t i{}; i != height; ++i) {
+    for (std::size_t j{}; j != width; ++j) {
+      state[off_x + i][off_y + j] = state_map[i][j];
+    }
+  }
+
+  return state;
+}
+
+auto count_surrounding(state_t const &state, std::size_t x, std::size_t y)
+    -> std::size_t
+{
+  std::size_t counter{};
+  for (int i{-1}; i != 2; ++i) {
+    for (int j{-1}; j != 2; ++j) {
+      if (x + i < state.size() && y + j < state[x + i].size() &&
+          (i != 0 || j != 0)) {
+        counter += state[x + i][y + j] == 'o' ? 1 : 0;
+      }
+    }
+  }
+  return counter;
+}
+
 auto main(int argc, char **argv) -> int
 {
   using namespace fast_io::io;
   using namespace fast_io::mnp;
+  using namespace std::chrono_literals;
 
-  if (argc < 2) {
-    println("Usage: game-of-life <initial-state-file>");
-    println("");
-    println("Format of the file is: 10x10 char array of '.' and 'o'.");
+  if (argc == 0) {
     return 1;
   }
 
-  std::span argv_s{argv, static_cast<std::size_t>(argc)};
+  if (argc < 2) {
+    println("Usage: ", os_c_str(*argv), " <config-file>");
+    println("");
+    println("Example can be found as state.zzh in project root directory.");
+    return 1;
+  }
 
-  auto *const state_filename{argv_s[1]};
+  fast_io::span argv_s{argv, static_cast<std::size_t>(argc)};
+
+  char const *const state_filename{argv_s[1]};
   fast_io::native_file_loader file_data{os_c_str(state_filename)};
-
   fast_io::ibuffer_view ibf{file_data};
-
-  std::size_t n;
-  std::size_t m;
-  scan(ibf, n, m);
 
   Settings settings{};
   scan(ibf, settings);
 
-  std::vector<std::string> state(n, std::string(m, '.'));
+  state_t state{initialize_state(settings, ibf)};
 
-  std::vector<std::string> initial_state;
-  std::string buf;
-  while (scan<true>(ibf, buf)) {
-    initial_state.push_back(buf);
-  }
-  auto const width{initial_state[0].size()};
-  auto const height{initial_state.size()};
-
-  // Initialize the state.
-  auto const off_x{(n - height) / 2};
-  auto const off_y{(m - width) / 2};
-  for (std::size_t i{}; i != height; ++i) {
-    for (std::size_t j{}; j != width; ++j) {
-      state[off_x + i][off_y + j] = initial_state[i][j];
-    }
-  }
-
-  using namespace std::chrono_literals;
   auto const wait_time{1000ms / settings.fps};
   auto last_start_point{std::chrono::steady_clock::now()};
   while (true) {
     // Render
     for (auto const &e : state) {
       for (auto const f : e) {
-        print(" ", chvw(f));
+        print(fast_io::out(), " ", chvw(f));
       }
-      println("");
+      println(fast_io::out(), "");
     }
-    println("");
+    println(fast_io::out(), "");
 
     // Update
-    auto count_surrounding{[](auto const &state, std::size_t x, std::size_t y) {
-      std::size_t counter{};
-      for (int i{-1}; i != 2; ++i) {
-        for (int j{-1}; j != 2; ++j) {
-          if (x + i < state.size() && y + j < state[0].size() &&
-              (i != 0 || j != 0)) {
-            counter += state[x + i][y + j] == 'o';
-          }
-        }
-      }
-      return counter;
-    }};
-
     auto next_cell_live{[&settings](bool on, std::size_t surrounding) {
-      if (on) {
-        return settings.on_minimum <= surrounding &&
-               surrounding <= settings.on_maximum;
-      }
-      return settings.off_minimum <= surrounding &&
-             surrounding <= settings.off_maximum;
+      return on ? settings.on_minimum <= surrounding &&
+                      surrounding <= settings.on_maximum
+                : settings.off_minimum <= surrounding &&
+                      surrounding <= settings.off_maximum;
     }};
 
     auto next_state{state};
